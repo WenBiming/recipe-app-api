54cc86b97326af968ae5785d55906006e8e45423
WenBiming <m18077021652@163.com>
2021-10-11 22:30:48 +0800

    Super user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,24 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        '''test the email for user is normalized'''
+        email = 'wbm@QQ.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='12456'
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_eamil(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'TEst123')
+
+    def test_create_new_super_user(self):
+        '''test creating new super user'''
+        user = get_user_model().objects.create_superuser(
+            email='wbm@qq.com',
+            password='123456'
+        )
+        self.assertTrue(user.is_staff)
